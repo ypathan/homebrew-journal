273f3838f8a94fdb065b6374fe8121b6ae68b14f
myousuf65 <official.yousufpathan@gmail.com>
2026-03-03 07:23:56 +0500

    adding the formula directory

---
+++ Formula/journal.rb
@@ -0,0 +1,39 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Journal < Formula
+  desc "CLI journal application written with bubble tea framework (inspired by yazi)"
+  homepage "https://github.com/ypathan/journal"
+  url "https://github.com/ypathan/journal/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "bbe68f03b0fd536a24ec305912eccb0bfcab6e41d519eb196ea408d7080e5a52"
+  license ""
+
+  depends_on "go" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "go", "build" , "."
+    bin.install "journal"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test journal`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
